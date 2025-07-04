6553850aa41d7c027ded37f5544fc4cee336c9d3
iChocoMilk <ichocodev@gmail.com>
2025-07-04 15:13:10 -0300

    add method to handle listener with custom EventExecutor

--- microkernel/api/src/main/java/ink/reactor/kernel/event/EventBus.java
+++ microkernel/api/src/main/java/ink/reactor/kernel/event/EventBus.java
@@ -8,6 +8,14 @@ public interface EventBus {
     void register(final Object listener);
 
     /**
+     * Register a listener with custom event executor,
+     * all process of executing the listener is handled by the event-executor
+     * @param listener object
+     * @param executor executor of the listener
+     */
+    void register(final Object listener, final EventExecutor executor);
+
+    /**
      * Unregister all methods that use {@link Listener} as annotation
      * @param listener object with listener methods
      */
